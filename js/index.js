32a114fd303c7debf88087a4ae38075ad7218e24
babypirate <62779686+babypirate@users.noreply.github.com>
2021-03-29 17:09:57 -0700

    V1

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function(){ $("#nav-placeholder").load("nav.html", {}, function() {
+  					const navToggle = document.querySelector('.nav-toggle');
+						const navLinks = document.querySelectorAll('.nav__link');
+
+						navToggle.addEventListener('click', () => {
+							document.body.classList.toggle('nav-open');
+						});
+
+						navLinks.forEach(link =>{
+							link.addEventListener('click', () => {
+								document.body.classList.remove('nav-open');
+							})
+						});
+					});
+				});
